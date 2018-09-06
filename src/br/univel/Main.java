de9f63f87d8b27c5e7583fc25d3525b42211b1e1
fernando.groeler <fernando.groeler@gmail.com>
2018-09-06 14:30:11 -0300

    adicionado mais informações.

--- src/br/univel/Main.java
+++ src/br/univel/Main.java
@@ -5,18 +5,43 @@ public class Main {
         BuscaLargura buscaLargura = new BuscaLargura();
         buscaLargura.criarGrafo();
 
-        long startTime = System.currentTimeMillis();
-        long startMemory = Runtime.getRuntime().freeMemory();
+        double totalTime = 0;
+        double totalMemory = 0;
+        int qtde = 0;
+        double mediaTempo = 0;
+        double mediaMemoria = 0;
 
-        buscaLargura.buscaPorLargura("1", "20");
 
-        long stopTime = System.currentTimeMillis();
-        long stopMemory = Runtime.getRuntime().totalMemory();
+        for (int i = 0; i < 10; i++) {
+            long startTime = System.currentTimeMillis();
+            long startMemory = Runtime.getRuntime().freeMemory();
 
-        long memorySpend = (stopMemory - startMemory);
-        long elapsedTime = (stopTime - startTime);
+            buscaLargura.buscaPorLargura("1", "20");
 
-        System.out.println(elapsedTime + " milisegundos");
-        System.out.println(memorySpend + " bits");
+            long stopTime = System.currentTimeMillis();
+            long stopMemory = Runtime.getRuntime().totalMemory();
+
+            long memorySpend = (stopMemory - startMemory);
+            long elapsedTime = (stopTime - startTime);
+
+            totalTime += elapsedTime;
+            totalMemory += memorySpend;
+            qtde += 1;
+
+            System.out.println(elapsedTime + " milisegundos");
+            System.out.println(memorySpend + " bits");
+        }
+
+        mediaTempo = (totalTime / qtde);
+        mediaMemoria =  (totalMemory / qtde);
+
+        System.out.println("----------------------------------------");
+        System.out.println("                 TOTAIS                 ");
+        System.out.println("----------------------------------------");
+        System.out.println("Tempo total.....: " + totalTime + " milisegundos");
+        System.out.println("Memória total...: " + totalMemory + " bits");
+        System.out.println("Média de tempo..: " + mediaTempo + " milisegundos");
+        System.out.println("Média de memória: " + mediaMemoria + " bits");
+        System.out.println("----------------------------------------");
     }
 }
